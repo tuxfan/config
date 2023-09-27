cf4175d4991952dd9cf2095685825706361711a2
Ben Bergen <bergen@lanl.gov>
2023-09-27 15:38:01 +0000

    Update

--- nvim/init.lua
+++ nvim/init.lua
@@ -12,3 +12,19 @@ vim.o.colorcolumn = '81'
 vim.o.cursorline  = true
 vim.o.hlsearch    = true
 vim.o.incsearch   = true
+
+-- Remember last cursor position
+local lastplace = vim.api.nvim_create_augroup("LastPlace", {})
+vim.api.nvim_clear_autocmds({ group = lastplace })
+vim.api.nvim_create_autocmd("BufReadPost", {
+    group = lastplace,
+    pattern = { "*" },
+    desc = "remember last cursor place",
+    callback = function()
+        local mark = vim.api.nvim_buf_get_mark(0, '"')
+        local lcount = vim.api.nvim_buf_line_count(0)
+        if mark[1] > 0 and mark[1] <= lcount then
+            pcall(vim.api.nvim_win_set_cursor, 0, mark)
+        end
+    end,
+})
