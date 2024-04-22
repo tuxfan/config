505dbd52955cc5895e67907a420fe77edd2bcfa2
Ben Bergen <bergen@lanl.gov>
2024-04-22 09:15:01 -0600

    Update

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,17 +1,18 @@
 require("config")
 
-vim.o.smarttab    = true
-vim.o.smartindent = true
-vim.o.expandtab   = true
-vim.o.linebreak   = true
-vim.o.tabstop     = 2
-vim.o.softtabstop = 2
-vim.o.shiftwidth  = 2
-vim.o.mouse       = 'r'
-vim.o.colorcolumn = '81'
-vim.o.cursorline  = true
-vim.o.hlsearch    = true
-vim.o.incsearch   = true
+vim.o.smarttab     = true
+vim.o.smartindent  = true
+vim.o.expandtab    = true
+vim.o.linebreak    = true
+vim.o.tabstop      = 2
+vim.o.softtabstop  = 2
+vim.o.shiftwidth   = 2
+vim.o.mouse        = 'r'
+vim.o.colorcolumn  = '81'
+vim.o.cursorline   = true
+vim.o.cursorcolumn = true
+vim.o.hlsearch     = true
+vim.o.incsearch    = true
 
 -- Remember last cursor position
 local lastplace = vim.api.nvim_create_augroup("LastPlace", {})
