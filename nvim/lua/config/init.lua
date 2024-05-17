3d021b5c1b34d7a6657c07c1e38336667e8026a2
Ben Bergen <bergen@lanl.gov>
2024-05-17 23:30:43 +0000

    New nvim configuration

--- nvim/lua/config/init.lua
+++ nvim/lua/config/init.lua
@@ -1,2 +1,16 @@
-require("config.remap")
-require("config.packer")
+require('config.keymaps')
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable', -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require('lazy').setup('config.lazy', opts)
+require('config.colorscheme')
---
+++ nvim/lua/config/lazy.lua
@@ -0,0 +1,37 @@
+return {
+  {
+    'nvim-telescope/telescope.nvim',
+    dependencies = { 'nvim-lua/plenary.nvim' }
+  },
+  {
+    "nvim-telescope/telescope-file-browser.nvim",
+    dependencies = {
+      {
+        'nvim-telescope/telescope.nvim',
+        'nvim-lua/plenary.nvim'
+      }
+    }
+  },
+  {
+    'nvim-treesitter/nvim-treesitter',
+    run = function()
+      local ts_update =
+        require('nvim-treesitter.install').update({ with_sync = true })
+      ts_update()
+    end
+  },
+  {
+    'folke/trouble.nvim',
+    dependencies = {
+      'nvim-tree/nvim-web-devicons'
+    }
+  },
+  {
+    'rose-pine/neovim',
+    as = 'rose-pine',
+    config = function()
+        vim.cmd('colorscheme rose-pine')
+    end
+  },
+  { 'theprimeagen/harpoon' }
+}
