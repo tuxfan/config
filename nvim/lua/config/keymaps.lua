2a23e897250a297694451b4f8a93f9028b2d9c25
Ben Bergen <bergen@lanl.gov>
2024-05-26 22:10:54 +0000

    Updates

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -28,3 +28,29 @@ vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
 -- NVim Tree
 ---------------------------------------------------------------------------------
 vim.keymap.set('n', '<c-n>', ':NvimTreeFindFileToggle<CR>')
+
+---------------------------------------------------------------------------------
+-- Fugitive
+---------------------------------------------------------------------------------
+vim.keymap.set('n', '<leader>gs', vim.cmd.Git)
+
+---------------------------------------------------------------------------------
+-- Undo
+---------------------------------------------------------------------------------
+vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle)
+
+---------------------------------------------------------------------------------
+-- Trouble
+---------------------------------------------------------------------------------
+vim.keymap.set('n', '<leader>xx',
+  function() require('trouble').toggle() end)
+vim.keymap.set('n', '<leader>xw',
+  function() require('trouble').toggle('workspace_diagnostics') end)
+vim.keymap.set('n', '<leader>xd',
+  function() require('trouble').toggle('document_diagnostics') end)
+vim.keymap.set('n', '<leader>xq',
+  function() require('trouble').toggle('quickfix') end)
+vim.keymap.set('n', '<leader>xl',
+  function() require('trouble').toggle('loclist') end)
+vim.keymap.set('n', 'gR',
+  function() require('trouble').toggle('lsp_references') end)
