93cf6ce1d959e10351ae693a7cd4e0b3a9754727
Ben Bergen <bergen@lanl.gov>
2024-06-09 15:09:32 +0000

    Updates

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -11,6 +11,7 @@ vim.keymap.set('n', '<leader>no', ':Neorg<CR>')
 vim.keymap.set('n', '<leader>ni', ':Neorg index<CR>')
 vim.keymap.set('n', '<leader>nr', ':Neorg return<CR>')
 vim.keymap.set('n', '<leader>nw', ':Neorg workspace')
+vim.keymap.set('n', '<localleader>nw', ':Neorg workspace')
 vim.keymap.set('n', '<localleader>nc', ':Neorg toggle-concealer<CR>')
 
 ---------------------------------------------------------------------------------
