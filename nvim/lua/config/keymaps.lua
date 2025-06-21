46e7efa453414dd80bc549c31f4935f3f9402ae1
Ben Bergen <bergen@lanl.gov>
2025-06-21 10:15:45 -0600

    Updates

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -33,14 +33,21 @@ vim.keymap.set('n', '<C-e>', ui.toggle_quick_menu)
 ---------------------------------------------------------------------------------
 -- Telescope
 ---------------------------------------------------------------------------------
-local builtin = require('telescope.builtin')
+local tele = require('telescope.builtin')
 
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fr', builtin.oldfiles, {})
-vim.keymap.set('n', '<leader>fs', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fc', builtin.grep_string, {})
-vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
+vim.keymap.set('n', '<leader>fb', tele.buffers, {})
+vim.keymap.set('n', '<leader>ff', tele.find_files, {})
+vim.keymap.set('n', '<leader>en',
+  function()
+    tele.find_files{
+      cwd = vim.fn.stdpath('config')
+    }
+  end
+)
+vim.keymap.set('n', '<leader>fr', tele.oldfiles, {})
+vim.keymap.set('n', '<leader>fs', tele.live_grep, {})
+vim.keymap.set('n', '<leader>fc', tele.grep_string, {})
+vim.keymap.set('n', '<leader>vh', tele.help_tags, {})
 
 ---------------------------------------------------------------------------------
 -- Fugitive
