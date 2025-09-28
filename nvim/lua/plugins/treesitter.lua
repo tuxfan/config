5a894b428244d570eb76dfe963075edcc9255e29
Ben Bergen <bergen@lanl.gov>
2025-09-28 12:16:38 -0600

    Updates

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -3,7 +3,7 @@ return {
   event = { 'BufReadPre', 'BufNewFile' },
   build = ':TSUpdate',
   dependencies = {
-    'OXY2DEV/markview.nvim',
+    --'OXY2DEV/markview.nvim',
     'windwp/nvim-ts-autotag'
   },
   config = function()
