e3970460b63847d7039e3fea39ecdab3edb92812
Ben Bergen <bergen@lanl.gov>
2025-10-08 14:15:43 -0600

    Updates

--- nvim-examples/nvim-bergen/lua/plugins/leap.lua
+++
@@ -1,6 +0,0 @@
-return {
---  {
---    'ggandor/leap.nvim',
---    dependencies = { 'tpope/vim-repeat' }
---  }
-}
--- nvim-examples/nvim-old/lua/config/remap.lua
+++
@@ -1,3 +0,0 @@
-vim.g.mapleader = ' '
-vim.keymap.set('n', '<leader>dl', vim.cmd.Ex)
-vim.keymap.set('n', '<leader>nh', function() vim.cmd('noh') end)
