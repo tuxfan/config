661784945197f063856284e85a921c6740a7f2b3
Ben Bergen <bergen@lanl.gov>
2026-04-04 09:28:35 -0600

    Updates

--- nvim/lua/plugins/obsidian.lua
+++ nvim/lua/plugins/obsidian.lua
@@ -18,7 +18,7 @@ vim.keymap.set('n', '<leader>oo', '<cmd>Obsidian quick_switch<CR>', { desc = 'Ob
 vim.keymap.set('n', '<leader>ow', '<cmd>Obsidian workspace<CR>', { desc = 'Obsidian change workspace' })
 vim.keymap.set('n', '<leader>on', '<cmd>Obsidian new<CR>', { desc = 'Obsidian new note' })
 vim.keymap.set('n', '<leader>ob', '<cmd>Obsidian backlinks<CR>', { desc = 'Obsidian backlinks' })
-vim.keymap.set('n', '<leader>oc', '<cmd>Obsidian check<CR>', { desc = 'Obsidian backlinks' })
+vim.keymap.set('n', '<leader>oc', '<cmd>Obsidian check<CR>', { desc = 'Obsidian check' })
 vim.keymap.set({ 'n', 'x' }, '<leader>ox', '<cmd>Obsidian toggle_checkbox<CR>', {
   desc = 'Obsidian cycle checkbox state',
 })
